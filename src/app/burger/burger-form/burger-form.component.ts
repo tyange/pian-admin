ec920296751ea222e83a5148c91c058e25780ed9
tyange <usun16@gmail.com>
2022-11-05 19:07:21 +0900

    burgers form component

---
+++ src/app/burger/burger-form/burger-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-burger-form',
+  templateUrl: 'burger-form.component.html',
+})
+export class BurgerFormComponent {}
--- src/app/burger/burger.component.ts
+++ src/app/burger/burger.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BurgerFormComponent } from './burger-form/burger-form.component';
 
 @Component({
   standalone: true,
   selector: 'app-burger',
   templateUrl: 'burger.component.html',
   styleUrls: ['burger.component.css'],
+  imports: [BurgerFormComponent],
 })
 export class BurgerComponent {}
