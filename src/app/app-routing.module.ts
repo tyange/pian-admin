569839214a56c242a05670dba2e338a08d230d65
tyange <usun16@gmail.com>
2022-11-16 07:21:13 +0900

    router 추가

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BurgerComponent } from './burger/burger.component';
+import { BurgerFormComponent } from './burger/burger-form/burger-form.component';
 
-const routes: Routes = [{ path: '', component: BurgerComponent }];
+const routes: Routes = [
+  {
+    path: 'burger/edit/:burgerId',
+    component: BurgerFormComponent,
+  },
+  { path: 'burger', component: BurgerComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
